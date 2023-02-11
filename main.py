021b1e5fe4537b622fa5be67cbd379e7ae69df85
fwoggodev <piecyanide0001@gmail.com>
2023-02-11 13:53:18 +0530

    Converted Code To RestfulAPI using Flask

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ import datetime
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from textblob import TextBlob
 
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
 
 filename = "journal.json"
 
-def add_entry():
+
+def add_entry(text):
     if os.path.exists(filename):
         with open(filename, "r") as file:
             journal = json.load(file)
@@ -16,19 +22,18 @@ def add_entry():
         today_entry = next((entry for entry in journal if entry['date_time'].startswith(today)), None)
 
         if today_entry:
-            print(f"Old text:\n{today_entry['text']}")
-            new_text = input("Enter new text to append:")
+            new_text = text
             today_entry['text'] = today_entry['text'] + "\n" + new_text
         else:
             today_entry = {
-                'text': input("Enter text for the day:"),
+                'text': text,
                 'date_time': str(datetime.datetime.now()),
                 'emotion': "",
             }
             journal.append(today_entry)
     else:
         journal = [{
-            'text': input("Enter text for the day:"),
+            'text': text,
             'date_time': str(datetime.datetime.now()),
             'emotion': "",
         }]
@@ -43,11 +48,10 @@ def add_entry():
     else:
         journal[0]['emotion'] = "Negative"
 
-    
     with open(filename, "w") as file:
         json.dump(journal, file, indent=4)
-    
-    print(f"Emotion: {journal[0]['emotion']}")
+
+    return journal[0]['emotion']
 
 def view_sentiment_calendar():
     if os.path.exists(filename):
@@ -79,24 +83,16 @@ def view_sentiment_calendar():
     else:
         print("No journal entries found.")
 
-def show_choices():
-    print("\n1. Add journal entry")
-    print("2. View sentiment analysis in a calendar form")
-    print("3. View Menu")
-    print("4. Exit")
+class Journal(Resource):
+    def post(self):
+        text = request.get_json()["text"]
+        if text:
+            emotion = add_entry(text)
+            return {"message": "Success", "emotion": emotion}, 200
+        else:
+            return {"error": "Text field is missing."}, 400
 
-show_choices()
-while True:
+api.add_resource(Journal, '/journal')
 
-    choice = int(input("Enter your choice:"))
-
-    if choice == 1:
-        add_entry()
-    elif choice == 2:
-        view_sentiment_calendar()
-    elif choice == 3:
-        show_choices()
-    elif choice == 4:
-        break
-    else:
-        print("Invalid choice.")
+if __name__ == '__main__':
+    app.run(debug=True,port=9999)
